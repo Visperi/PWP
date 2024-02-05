4e3ed8a92804afb728dcef629886f7bd0b8146b4
Juho B <jopbruun@gmail.com>
2024-02-05 22:39:04 +0200

    oops

--- models.py
+++ models.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 class Player(db.Model):
     __tablename__ = "players"
