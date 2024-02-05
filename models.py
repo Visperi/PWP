3d481791a58d4976229d3db846166f5e67e72bb8
Juho B <jopbruun@gmail.com>
2024-02-05 22:33:36 +0200

    initial commit

---
+++ models.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Player(db.Model):
+    __tablename__ = "players"
+    username = db.Column(db.String(32), primary_key=True, nullable=False, unique=True)
+    num_of_matches = db.Column(db.Integer, default=0, nullable=False)
+    rating = db.Column(db.Integer, default=1000, nullable=False)
+    matches = db.relationship("MatchPlayerRelation", back_populates="player", lazy='select')
+
+class Match(db.Model):
+    __tablename__ = "matches"
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(100))
+    status = db.Column(db.Integer, default=0, nullable=False)
+    rating_shift = db.Column(db.Integer)
+    team1_score = db.Column(db.Integer, default=0)
+    team2_score = db.Column(db.Integer, default=0)
+    players = db.relationship('MatchPlayerRelation', back_populates='match', lazy='select')
+
+class MatchPlayerRelation(db.Model):
+    __tablename__ = "match_player_relation"
+    username = db.Column(db.String(32), db.ForeignKey("players.username", ondelete='CASCADE'), nullable=False, primary_key=True)
+    match_id = db.Column(db.Integer, db.ForeignKey("matches.id", ondelete='CASCADE'), nullable=False, primary_key=True)
+    team = db.Column(db.Integer, nullable=False)
+    player = db.relationship("Player", back_populates="matches")
+    match = db.relationship("Match", back_populates="players")
+    
