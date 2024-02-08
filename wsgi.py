b4ae7abbe813731bf06bf754829a15c4b272a81e
Visperi <niko.matasaho@gmail.com>
2024-02-09 01:38:21 +0200

    Add WSGI file
    Add file for one common place to start the app

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+"""
+Module for starting the application. Either run this module as the main module for development mode or call the
+create_app method to get a ready-to-use Flask object for Ranking API.
+"""
+from ranking_api.app import create_app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True)
