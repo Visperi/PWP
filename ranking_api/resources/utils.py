2dec6d7a9a8efcbbfd798f77b2f11851562a5cce
Visperi <niko.matasaho@gmail.com>
2024-03-01 14:20:27 +0200

    Add docstrings, change variable type
    Change compared variable type from casefold to lower, since it is more familiar and effectively the same

--- ranking_api/resources/utils.py
+++ ranking_api/resources/utils.py
@@ -1,12 +1,23 @@
+"""
+Utility module for processing HTTP methods for resources.
+"""
+
 from werkzeug.exceptions import BadRequest
 
 
-def str_to_bool(value: str):
-    casefold = value.casefold()
+def str_to_bool(value: str) -> bool:
+    """
+    Convert a query parameter to boolean variable and raise an exception if it is not boolean type.
 
-    if casefold == "true":
+    :param value: The query parameter value
+    :return: True if the parameter lowered is 'true' or False if it is 'false'
+    :raises BadRequest: If the parameter value is invalid, i.e. is not strictly a boolean in string format.
+    """
+    lower = value.lower()
+
+    if lower == "true":
         return True
-    elif casefold == "false":
+    elif lower == "false":
         return False
     else:
         raise BadRequest(description="Bad query parameter")
